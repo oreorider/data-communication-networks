22cec4f82f55beb0e8e5137ae93b1556c3c2c13b
Kyle <yjkim308@gmail.com>
2022-12-23 11:56:10 +0900

    ddd

--- project3/server.c
+++ project3/server.c
@@ -1,4 +1,6 @@
 #define _GNU_SOURCE 1
+#define ENABLECODE 1
+#define DISABLECODE 0
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,9 +14,70 @@
 #define PORT 7777
 #define BUFFER_SIZE 1000000
 
-void forward (int sock, int* connectfail);
+void forward (int sock);
 void forward_2(int sock);
 
+
+#if ENABLECODE
+int main( int argc, char *argv[] ) {
+  int sockfd, newsockfd, portno = PORT;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+  clilen = sizeof(cli_addr);
+
+  /* First call to socket() function */
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sockfd < 0) {
+    perror("ERROR opening socket");
+    exit(1);
+  }
+
+  
+  // port reusable
+  int tr = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &tr, sizeof(int)) == -1) {
+    perror("setsockopt");
+    exit(1);
+  }
+
+  /* Initialize socket structure */
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  
+  /* Now bind the host address using bind() call.*/
+  if ( bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1 ){
+    perror("bind error");
+    exit(1);
+  }
+
+  /* listen on socket you created */
+  if ( listen(sockfd, 10) == -1 ){
+    perror("listen error");
+    exit(1);
+  }
+
+  printf("Server is running on port %d\n", portno);
+
+  while (1) {
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    
+    if ( newsockfd == -1 ){
+      perror("accept error");
+      exit(1);
+    }
+    forward(newsockfd);
+  }
+
+  return 0;
+}
+
+#endif
+
+#if !ENABLECODE
 int main( int argc, char *argv[] ) {
   int sockfd, newsockfd, portno = PORT;
   socklen_t clilen;
@@ -94,6 +157,7 @@ int main( int argc, char *argv[] ) {
 
   return 0;
 }
+#endif
 
 /* 
   TODO (1): Forward received HTTP request to the web server
@@ -113,7 +177,7 @@ int main( int argc, char *argv[] ) {
       
  */
 
-void forward(int sock, int *connectfail) {
+void forward(int sock) {
   int offset, bytes;
   char buffer[BUFFER_SIZE];
   bzero(buffer,BUFFER_SIZE);
@@ -132,12 +196,10 @@ void forward(int sock, int *connectfail) {
 
   if (offset < 0) {
     printf("recv() error\n");
-    *connectfail = 1;
     return;
-  } else if (bytes == 0) {
+  }
+  else if (bytes == 0) {
     printf("RECEIVE FROM PHONE Client disconnected unexpectedly\n");
-    sock = -1;
-    *connectfail = 1;
     return;
   }
   
@@ -187,12 +249,9 @@ void forward(int sock, int *connectfail) {
     //string manipulation, remove domain, leave only the thing after / ex) /index.html
     char* after_slash_ptr = strstr(buffer, ":9000") + 5;
     char* before_http_ptr = strstr(buffer, "HTTP/1.1")-1;
-    //int size_of_message = offset;
     char details[BUFFER_SIZE];
     bzero(details, BUFFER_SIZE);
     int detail_length = before_http_ptr - after_slash_ptr;
-    //printf("detail length is %d\n", detail_length);
-
     int index = 0;
     while(after_slash_ptr != before_http_ptr){
       details[index] = *after_slash_ptr;
@@ -201,67 +260,36 @@ void forward(int sock, int *connectfail) {
     }
     details[detail_length] = '\0';
     printf("detail is >%s<\n", details);
-
     char* newline_ptr = strstr(buffer, "\r");
-    //char* buffer_end = strstr(buffer, "\r\n\r\n")+4;
     char* buffer_end = buffer+offset;//pointer to the element next to the last 
-
     char remainder[BUFFER_SIZE];
     bzero(remainder, BUFFER_SIZE);
-    /*
-    index=0;
-    for(char* chptr = newline_ptr; chptr != buffer_end+1; chptr++){
-      remainder[index] = *chptr;
-      index+=1;
-    }*/
-
     strncpy(remainder, newline_ptr, buffer_end-newline_ptr);
     buffer_end = "\0";
-    //char message_to_send[BUFFER_SIZE];
-    //bzero(message_to_send, BUFFER_SIZE);
-
-    //strcat(message_to_send, "GET ");
-    //strcat(message_to_send, details);
-    //strcat(message_to_send, " HTTP/1.1");
-    //strcat(message_to_send, remainder);
-
     char *message_to_send;
     asprintf(&message_to_send, "%s%s%s%s", "GET ", details, " HTTP/1.1", remainder);
-    //printf("AHHHHHHHHHHHHHHHHHHHHHHHHHHHH\n%.*s\n", offset-26, message_to_send);
-
-    //buffer_end = "\0";
-    //char* messsage_to_send;
-    //asprintf(&message_to_send, "%s %s %s%s", "GET", details, "HTTP/1.1", remainder);
-    //string parsing end  
-    //for(int i=0; i<strlen(message_to_send); i++){
-    //  printf("%02x ", message_to_send[i]);
-    //}
     printf("\n");
-
-    /*
-    bytes = send(sockfd, buffer, strlen(message_to_send), 0);
-    printf("sent bytes : %d\n\n", bytes);
-    printf("=============message to send to nxcserver============\n\n%s", message_to_send);
-    */
-
     int length = strlen(message_to_send);
-
     printf("length is %d\n", length);
 
     //send the message
+    bytes = send(sockfd, message_to_send, strlen(message_to_send), 0);
+    //printf("message sent to server is:\n%s\n",message_to_send);
+    printf("sent bytes : %d\n", bytes);
     
-    /*while(length > 0){
+    /*
+    while(length > 0){
       bytes = send(sockfd, message_to_send, length, 0);
       printf("send bytes : %d\n", bytes);
       length = length - bytes;
     }*/
-    bytes = send(sockfd, message_to_send, length, 0);
-    printf("=============message to send to nxcserver============\n\n%s", message_to_send);
+
+    //shutdown(sockfd, SHUT_WR);
     //bytes = send(sockfd, message_to_send, length, 0);
+    printf("=============message to send to nxcserver============\n\n%s", message_to_send);
     printf("===========message sent to nxc server============\n\n");
     
     //read incoming packet from server to computer
-    printf("==========receiving message from nxc server===========\n");
     char receive_buffer[BUFFER_SIZE];
     bzero(receive_buffer, BUFFER_SIZE);
     
@@ -270,52 +298,62 @@ void forward(int sock, int *connectfail) {
     
     char* badreq;
     offset = 0;
-    //attempted timeout on read... doesnt work
-    /*
-    int rx_len;
+
+    //attempted timeout on read...
+    int ret;
     struct timeval timeout;
-    fd_set readFds;
-
     timeout.tv_sec = 0;
-    timeout.tv_usec = 1000000;
-
-    FD_ZERO(&readFds);
-    FD_SET(sockfd, &readFds);
-    select(sockfd+1, &readFds, NULL, NULL, &timeout);
-
+    timeout.tv_usec = 100000;
+    
+    fd_set readFds;
+    
     do{
-      //printf("REEEEEEEEEEEEE\n");
       //configure read timeout
-      
-      //printf("REEEEEEEEEEEEE\n");
-      select(sockfd+1, &readFds, NULL, NULL, &timeout);
-      if(FD_ISSET(sockfd, &readFds)){
-        bytes = read(sockfd, receive_buffer+offset, BUFFER_SIZE);
+      FD_ZERO(&readFds);
+      FD_SET(sockfd, &readFds);
+      printf("\nwait for server response\n");
+      //make a timeout for read, so if nothing is sent after timeout time, it exits the loop
+      ret = select(sockfd+1, &readFds, NULL, NULL, &timeout);
+      if(ret == -1){
+        perror("select");
+        exit(1);
       }
-      if(bytes > 0) {
-        //printf("RRRRRRRRRRRRRRRRRRRE\n");
-        //bytes = read(sockfd, receive_buffer+offset, BUFFER_SIZE);
-        //if(bytes == 0) break;
-        offset+=bytes;
-        printf("\nreading %d bytes from from socket\noffset is %d\n", bytes, offset);
-        if(bytes < 2000) printf("=======================================================buffer contents\n\n%s\n", receive_buffer+offset-bytes);
-        else printf("too long, cant be bothered\n");
-        if(strncmp(receive_buffer+offset-4, "\r\n\r\n", 4) == 0) break;
-        //printf("ahahh\n");
-        //if bad request comes in, just ignore
-        if((badreq = strstr(receive_buffer+offset-bytes, "HTTP/1.1 400")) != NULL) {
-          printf("bad request found, exiting\n");
-          offset = badreq -receive_buffer;
-          break;
-        }
-      }
-      else{
-        printf("message timeout sending\n");
+      else if(ret == 0){
+        printf("timeout~~~~~~~~~\n");
+        //sleep(5);
+
         break;
       }
+      else{
+        //if ret returned > 0, ret socket(s) have data in them
+        if(FD_ISSET(sockfd, &readFds)){
+          printf("%d socket(s) available to read\n", ret);
+          printf("==========receiving message from nxc server===========\n");
+          bytes = read(sockfd, receive_buffer+offset, BUFFER_SIZE);
+          offset+=bytes;
+
+          printf("reading %d bytes from from socket\noffset is %d\n", bytes, offset);
+          //if(bytes < 2000) printf("\n\n%s\n", receive_buffer+offset-bytes);
+          //else printf("too long, cant be bothered\n");
+          if(strncmp(receive_buffer+offset-4, "\r\n\r\n", 4) == 0) {
+            printf("HTTP end marker found, exiting\n");
+            break;
+          }
+          
+          //if bad request comes in, just ignore, change offset appropriately
+          if((badreq = strstr(receive_buffer+offset-bytes, "HTTP/1.1 400")) != NULL) {
+            printf("bad request found, exiting\n");
+            offset = badreq -receive_buffer;
+            break;
+          }
+        }
+        
+      }
       //if(strstr(receive_buffer, "\r\n\r\n")!= NULL)break;
-    }while (bytes > 0);
-    */
+    }while (1);
+    
+    
+    /*
     do{
       //printf("RRRRRRRRRRRRRRRRRRRE\n");
       bytes = read(sockfd, receive_buffer+offset, BUFFER_SIZE);
@@ -333,42 +371,47 @@ void forward(int sock, int *connectfail) {
         break;
       }
     
-    }while (bytes > 0);
+    }while (bytes > 0);*/
+    
     /*
     do{
-      bytes = read(sockfd, receive_buffer, BUFFER_SIZE);
+      bytes = read(sockfd, receive_buffer+offset, BUFFER_SIZE);
       offset += bytes;
       printf("message received\n");
       printf("receive bytes : %d\n", bytes);
-      if(bytes>5000){
-        printf("dont bother printing\n");
-      }
-      else{
-        printf("buffer content\n%.*s\n", offset, receive_buffer);
-      }
-    }while(bytes > 0);
-    */
-    //printf("%s\n", receive_buffer);
+      printf("buffer content\n%.*s\n", offset, receive_buffer);
+      if (strncmp(buffer + offset - 4, "\r\n\r\n", 4) == 0) break;
+    }while(bytes > 0);*/
+
+    /*
+    int finished = 0;
+    bytes = read(sockfd, receive_buffer, BUFFER_SIZE);    
+    printf("PRINT RECEIVE BUFFER\n\n%s\n", receive_buffer);
+    offset += bytes;
+    if(bytes < 1000) finished = 1;
     
+    if(strstr(receive_buffer, "Partial Content") != NULL){
+      do{
+        bytes = read(sockfd, receive_buffer+offset, BUFFER_SIZE);
+        offset+=bytes;
+      }while(bytes>0);
+    }*/
+
     if(bytes < 0){
       perror("receive error");
       exit(1);
     }
     else if (bytes == 0){
-      perror("READING FROM NXCclient disconneced unexpectedly");
-      exit(1);
+      //perror("READING FROM NXCclient disconneced unexpectedly");
+      1;//do nothing, just here for lols
+      //exit(1);
     }
-
-    //receive_buffer[offset+1] = '\0';
-    //char printbuffer[9000];
-    //strncpy(printbuffer, buffer, bytes);
-    //printbuffer[bytes] = '\0';
-    //printf("MESSAGE FROM NXC SERVER\n\n%s||||", receive_buffer);
+  
     
     printf("========received message from nxc server==============\n\n");
 
     //close connection with nxc server
-    printf("socket with nxc server closing\n");
+    printf("\nsocket with nxc server closing\n");
     shutdown(sockconnection, SHUT_RDWR);
     close(sockconnection);
     printf("socket with nxc server closed\n\n");
@@ -419,9 +462,9 @@ void forward(int sock, int *connectfail) {
   }
   
   else{
-    //close(sock);
-    //printf("close phone laptop socket connection\nbecause it wasn't nxclab\n");
-    printf("not nxclab, exit to main==========================================\n");
+    close(sock);
+    printf("close phone laptop socket connection because it wasn't nxclab\n");
+    printf("exit to main==========================================\n");
   }
   /*
 
